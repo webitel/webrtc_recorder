d9d4d496aeda0b73c528e07f9687cf87dd20f807
i.navrotskyj <i.navrotskyj@gmail.com>
2025-07-26 13:58:47 +0300

    README.md

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/webitel/webrtc_recorder/cmd"
 )
 
+//go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate --template buf/buf.gen.engine.yaml
+//go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate --template buf/buf.gen.storage.yaml
+//go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate --template buf/buf.gen.webitel.yaml
+//go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate --template buf/buf.gen.yaml
+
 //go:generate go run github.com/google/wire/cmd/wire@latest gen ./cmd
 func main() {
 	if err := cmd.Run(); err != nil {
